bb09c728cd70c8824bca3996e33ac0a221e6990f
Alex <alexheistdev@gmail.com>
2018-05-31 12:31:32 -0600

    Django Shop

--- mydj/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Profile, Store, Booth, Exhibitor
-
-# Register your models here.
-admin.site.register(Profile)
-admin.site.register(Store)
-admin.site.register(Booth)
-admin.site.register(Exhibitor)
